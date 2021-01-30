b45a76e7101deba2467404335cd7fffbd29774ca
Hadi Ahmed <hadiahmed098@gmail.com>
2021-01-29 23:19:27 -0500

    Cleared repo for new implementation

--- http_file_script.py
+++
@@ -1,8 +0,0 @@
-import urllib3
-http = urllib3.PoolManager() # Threadsafe connection manager
-response = http.request('GET', 'http://localhost:8080/transfer.part')  # Get the container
-filename = 'transfer_test.part'
-file = open(filename, 'wb')
-file.write(response.data)  # Write the container
-print("wrote", filename)
-file.close()
