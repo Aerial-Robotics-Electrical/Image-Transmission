a3aa3fa524b4aacc0a76cd6c8e7e45cca6837cda
Hadi Ahmed <hadiahmed098@gmail.com>
2021-01-31 00:34:06 -0500

    Added image transmit functionality

--- image_transmit_client.py
+++ image_transmit_client.py
@@ -1,6 +1,6 @@
 import grpc
-import image_transmit_pb2_grpc as pb2_grpc
-import image_transmit_pb2 as pb2
+import ImageTransmission.image_transmit_pb2_grpc as pb2_grpc
+import ImageTransmission.image_transmit_pb2 as pb2
 
 
 class ImageTransmitClient(object):
@@ -10,17 +10,29 @@ class ImageTransmitClient(object):
         self.server_port = 50051
 
         # instantiate a channel
-        self.channel = grpc.insecure_channel('{}:{}'.format(self.host, self.server_port))
+        self.channel = grpc.insecure_channel(f'{self.host}:{self.server_port}')
 
         # bind the client and server
-        self.stub = pb2_grpc.TransmitTimeAndGPSStub(self.channel)
+        self.stub = pb2_grpc.TransmitImageAndDataStub(self.channel)
 
-    def get_data(self, id):
-        message = pb2.TimeGPSRequest(request_id=id)
+    def get_data(self, r_id):
+        message = pb2.ImageDataRequest(request_id=r_id)
         return self.stub.SendData(message)
+
+
+def decode_data(response):
+    if response.status_code:
+        print(f'request {response.request_id} returned a non-zero status code')
+    with open('tests/metadata.txt', 'w') as file:
+        file.write(f'request_id: {response.request_id}\n')
+        file.write(f'time_stamp: {response.time_stamp}\n')
+        file.write(f'nmea_gprmc: {response.nmea_gprmc}\n')
+    with open('tests/received_img.jpg', 'wb') as file:
+        file.write(response.image_data)
+    print(f'request {response.request_id} received')
 
 
 if __name__ == '__main__':
     client = ImageTransmitClient()
-    result = client.get_data(id='testid')
-    print(f'{result}')
+    result = client.get_data(r_id='req_0')
+    decode_data(result)
--- image_transmit_server.py
+++ image_transmit_server.py
@@ -1,11 +1,11 @@
 import grpc
 from concurrent import futures
 import time
-import image_transmit_pb2_grpc as pb2_grpc
-import image_transmit_pb2 as pb2
+import ImageTransmission.image_transmit_pb2_grpc as pb2_grpc
+import ImageTransmission.image_transmit_pb2 as pb2
 
 
-class ImageTransmitServer(pb2_grpc.TransmitTimeAndGPSServicer):
+class ImageTransmitServer(pb2_grpc.TransmitImageAndDataServicer):
 
     def __init__(self, *args, **kwargs):
         pass
@@ -13,14 +13,35 @@ class ImageTransmitServer(pb2_grpc.TransmitTimeAndGPSServicer):
     def SendData(self, request, context):
         # get the string from the incoming request
         message = request.request_id
-        result = {'request_id': message, 'timestamp': str(time.time()), 'gpscoords': 'some coords here'}
+
+        # TODO set the status code (0 for success)
+        st_code = 0
+
+        # TODO add some way to grab gps data here (NMEA?)
+        # Use GPRMC for compatibility
+        gps_coords = '$GPRMC,042608.994,A,4025.718,N,08654.827,W,,,310121,000.0,W*6B'
+
+        # TODO set the timestamp of the image
+        time_stamp = str(time.time())
+
+        # TODO load and set image bytes
+        # FIXME use a with statement here
+        file = open('tests/test_img.jpg', 'rb')
+        image_bytes = file.read()
+        file.close()
+
+        response = {'request_id': message, 'status_code': st_code,
+                    'time_stamp': time_stamp, 'nmea_gprmc': gps_coords,
+                    'image_data': image_bytes}
         print(f'Handling request {message}')
-        return pb2.TimeGPSResponse(**result)
+        return pb2.ImageDataResponse(**response)
 
 
 def serve():
-    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    pb2_grpc.add_TransmitTimeAndGPSServicer_to_server(ImageTransmitServer(), server)
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=5))
+    # Shouldn't be making more than 1 or 2 request at a time
+
+    pb2_grpc.add_TransmitImageAndDataServicer_to_server(ImageTransmitServer(), server)
     server.add_insecure_port('[::]:50051')
     server.start()
     server.wait_for_termination()
